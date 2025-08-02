05d1bb2e5507ab6f037493c411182b7bc8a1bf61
Krishnaraj-Irkal <krishnarajirkal@gmail.com>
2025-08-02 22:29:21 +0530

    completed signin page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -24,7 +25,7 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
         const { password: pass, ...rest } = validUser._doc;
         res
-            .cookie("access_token", token, { httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60) })
+            .cookie("access_token", token, { httpOnly: true })
             .status(200).json(rest)
     } catch (error) {
         next(error);
